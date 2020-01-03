77bee2c2a0b8c51d938856f702b840e7ebb29844
Nick930826 <xianyou1993@qq.com>
2020-01-03 14:02:37 +0800

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  deploy: {
+    production: {
+      user: 'root',
+      host: '47.99.134.126',
+      ref: 'origin/master',
+      repo: 'git@github.com:Nick930826/nick-blog-admin.git',
+      path: '/workspace/nick-blog-admin',
+      'post-deploy': 'git reset --hard && git checkout master && git pull && rm -rf build && npm install && npm run build && pm2 reload server.js',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
