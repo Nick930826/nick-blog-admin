2761213ae4b4e131a979ff5eb0f71be7c63cc2b6
Nick930826 <xianyou1993@qq.com>
2020-01-03 14:16:11 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,25 @@
 module.exports = {
+  apps: [
+    {
+      name: 'nick-blog-admin',
+      script: 'server.js',
+      exec_mode: 'cluster',
+      instances: process.argv.includes('beta') ? 1 : 'max',
+      min_uptime: '60s', // 应用运行少于时间被认为是异常启动
+      max_restarts: 30, // 最大异常重启次数，即小于min_uptime运行时间重启次数；
+      autorestart: true, // 默认为true, 发生异常的情况下自动重启
+      env: {
+        NODE_ENV: 'production'
+      },
+      env_beta: {
+        NODE_ENV: 'production',
+        isBeta: 'true'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
   deploy: {
     production: {
       user: 'root',
@@ -6,7 +27,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:Nick930826/nick-blog-admin.git',
       path: '/workspace/nick-blog-admin',
-      'post-deploy': 'git reset --hard && git checkout master && git pull && rm -rf build && npm install && npm run build && pm2 restart server.js',
+      'post-deploy': 'git reset --hard && git checkout master && git pull && rm -rf build && npm install && npm run build && pm2 startOrReload ecosystem.config.js',
       env: {
         NODE_ENV: 'production'
       }
