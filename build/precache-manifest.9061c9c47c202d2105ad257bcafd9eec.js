5fbcb8cff4a08e62d0a179d41decb10bf96253c6
Nick930826 <xianyou1993@qq.com>
2020-03-31 17:35:34 +0800

    update

---
+++ build/precache-manifest.9061c9c47c202d2105ad257bcafd9eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3145fb802aa8aac052e9107ab8965c59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d669d74875be365216fd",
+    "url": "/static/css/2.c7b90ac4.chunk.css"
+  },
+  {
+    "revision": "9369c14b6ca5b1a18b82",
+    "url": "/static/css/main.f1b60985.chunk.css"
+  },
+  {
+    "revision": "d669d74875be365216fd",
+    "url": "/static/js/2.e5c7b0c8.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.e5c7b0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9369c14b6ca5b1a18b82",
+    "url": "/static/js/main.c69bb07b.chunk.js"
+  },
+  {
+    "revision": "57bcd4a9e89d9e3659f6",
+    "url": "/static/js/runtime-main.6db3b199.js"
+  }
+]);
